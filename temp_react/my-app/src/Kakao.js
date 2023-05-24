56ed05ee297b73470a3da252a062cc0d3c7782a4
dnjstjt1297 <dnjstjt1297@gmail.com>
2023-05-24 15:42:58 +0900

    user location(watchPosition)

--- temp_react/my-app/src/Kakao.js
+++ temp_react/my-app/src/Kakao.js
@@ -1,18 +1,48 @@
 
-import { useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { db } from "./Firebase";
 import "firebase/firestore"; 
 
 import { collection,getDocs } from 'firebase/firestore/lite';
 import useGeolocation from "react-hook-geolocation";
-import userLocation from "./userLocation";
-
+import {getDistance,setLocationList,setRecording,setcoords,setWatchId} from "geolib";
 
 const {kakao} = window;
 function Kakao (){
 
   useEffect(()=>{ mapscript();},[])
   
+
+  const [loctaion, setLocation] = useState({});
+  const [error, setError] = useState(null);
+  let options;
+  const locationWatchId = useRef(null);
+
+
+  const handleSuccess = (pos)=>{
+    const{latitude,longitude} = pos.coords;
+
+    setLocation({latitude, longitude}) // 현재위치
+  }
+
+  const handleError = (error) =>{
+    setError(error.message);
+  }
+  options = {
+    enableHighAccuracy: false,
+    timeout: 5000,
+    maximumAge: 0,
+  }
+
+  useEffect(() => {
+    const { geolocation } = navigator;
+    if (!geolocation) {
+      setError("Geolocation is not supported.");
+      return;
+    }
+    locationWatchId.current = geolocation.watchPosition(handleSuccess,handleError,options);
+  },[options]);
+
   const mapscript = () => {
   const container = document.getElementById('map');
     const options = {
@@ -30,36 +60,29 @@ function Kakao (){
       })
     })
     if (navigator.geolocation) {
-
-      navigator.geolocation.getCurrentPosition(function(position) {
-        const mylat = position.coords.latitude; 
-        const mylon = position.coords.longitude; 
-        const locPosition = new kakao.maps.LatLng(mylat, mylon); 
-        var backMarkerImage = new kakao.maps.MarkerImage("image/myloc.png",
-        new kakao.maps.Size(32, 40),
-        {
-            offset: new kakao.maps.Point(15, 29)
-        }); 
-        console.log(mylat,mylon);
-        new kakao.maps.Marker({
-          map:map,
-          position: locPosition,
-          title:"mylocation",
-          image: backMarkerImage,
-        })
-
-      });
-      <userLocation/>
+      const locPosition = new kakao.maps.LatLng(loctaion.latitude,loctaion.longitude);
+      var backMarkerImage = new kakao.maps.MarkerImage("image/myloc.png",
+      new kakao.maps.Size(32, 40),
+      {
+        offset: new kakao.maps.Point(15, 29)
+      }); 
+      console.log(loctaion);
+      new kakao.maps.Marker({
+        map:map,
+        position: locPosition,
+        title:"mylocation",
+        image: backMarkerImage,
+      })
     }
-    
-    }
+  }
     
   
   return (
     <div id="map" style={{
       width:'75vh',
       height:'75vh'
-    }}></div>
+    }}>
+    </div>
     
 
   )
