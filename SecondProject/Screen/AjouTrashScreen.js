ade28e648b4cf2cf6c2669e4dc8b6b3d9969f5b2
ms-7365 <gusalstn91@gmail.com>
2023-05-31 22:09:25 +0900

    trash add

--- SecondProject/Screen/AjouTrashScreen.js
+++ SecondProject/Screen/AjouTrashScreen.js
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { Platform, Text, View, StyleSheet, Dimensions, PermissionsAndroid } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
+import firestore from "@react-native-firebase/firestore";
 import NaverMapView, { Marker } from 'react-native-nmap';
+export const TrashXY = firestore().collection("trash");
+
+var distance = 1e9;
+var title = "111";
+var globalVariable=0;
+
 
 function AjouTrashScreen() {
   const [hasLocationPermission, setHasLocationPermission] = useState(false);
   const [currentLatitude, setCurrentLatitude] = useState(null);
   const [currentLongitude, setCurrentLongitude] = useState(null);
+  const [trash_lat,setTrash_lat] = useState(0);
+  const [trash_lng,setTrash_lng] = useState(0);
 
   useEffect(() => {
     requestLocationPermission();
@@ -56,8 +65,23 @@ function AjouTrashScreen() {
   }, [hasLocationPermission]);
 
   const initCoords = { latitude: 37.2822222, longitude: 127.04410553 };
+  if(currentLongitude && currentLatitude)
+  {
+        TrashXY.get().then((res) => {
+          res.forEach((doc) => {
+            if (distance > ((doc.data().lat - currentLatitude) ** 2 + (doc.data().lng - currentLongitude) ** 2)) {
+              title = doc.data().title;
+              temp_lat = doc.data().lat;
+              temp_lng = doc.data().lng;
+              distance = ((doc.data().lat - currentLatitude) ** 2 + (doc.data().lng - currentLongitude) ** 2);
+            }
+          });
+        setTrash_lat(temp_lat);
+        setTrash_lng(temp_lng);
+        });
+  }
 
-  return (
+return (
     <View style={styles.container}>
       <Text>
         {currentLatitude} {currentLongitude}
@@ -67,13 +91,18 @@ function AjouTrashScreen() {
           {currentLatitude && currentLongitude && (
             <Marker coordinate={{ latitude: currentLatitude, longitude: currentLongitude }} />
           )}
+          <Marker coordinate={{ latitude: trash_lat, longitude: trash_lng }} />
         </NaverMapView>
       ) : (
         <Text>Please grant location permission to show the map.</Text>
       )}
     </View>
   );
+
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
