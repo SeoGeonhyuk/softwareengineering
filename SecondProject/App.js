58d67829c8d37feac20038f40969c3b825644fc6
ori-web <sgh4123@naver.com>
2023-06-01 16:54:01 +0900

    add splash screen

--- SecondProject/App.js
+++ SecondProject/App.js
@@ -1,7 +1,8 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, Button, Platform, Alert, BackHandler } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from 'react-native-splash-screen';
 import HomeScreen from './Screen/HomeScreen';
 import AjouTrashScreen from './Screen/AjouTrashScreen';
 import Ajou3DScreen from './Screen/Ajou3DScreen';
@@ -12,16 +13,21 @@ import Eulgok from './Screen/Ajou3D/Eulgok';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  React.useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 3000);
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="AjouTrash" component={AjouTrashScreen} />
         <Stack.Screen name="Ajou3D" component={Ajou3DScreen} />
-        <Stack.Screen name="Sanhak" component={Sanhak}/>
-        <Stack.Screen name="Eulgok" component={Eulgok}/>
-        <Stack.Screen name="Paldal" component={Paldal}/>
-        <Stack.Screen name="Seongho" component={Seongho}/>
+        <Stack.Screen name="Sanhak" component={Sanhak} />
+        <Stack.Screen name="Eulgok" component={Eulgok} />
+        <Stack.Screen name="Paldal" component={Paldal} />
+        <Stack.Screen name="Seongho" component={Seongho} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- SecondProject/android/app/src/main/java/com/secondproject/MainActivity.java
+++ SecondProject/android/app/src/main/java/com/secondproject/MainActivity.java
@@ -1,12 +1,20 @@
 package com.secondproject;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
-public class MainActivity extends ReactActivity {
+import org.devio.rn.splashscreen.SplashScreen;
 
+public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState){
+      SplashScreen.show(this);
+      super.onCreate(savedInstanceState);
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -32,4 +40,5 @@ public class MainActivity extends ReactActivity {
         DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
         );
   }
+
 }
